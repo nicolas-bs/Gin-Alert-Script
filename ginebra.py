4c3a4edb73874e58641462df5a90a17d1a27b564
nicolas-bs <69317512+nicolas-bs@users.noreply.github.com>
2023-03-23 10:37:36 -0500

    Update ginebra.py

--- ginebra.py
+++ ginebra.py
@@ -43,12 +43,11 @@ def get_driver():
     prefs = {"profile.managed_default_content_settings.images": 2}
     options.add_experimental_option("prefs", prefs)
 
-    #driver_service = Service(ChromeDriverManager().install())
+    driver_service = BraveService(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install())
     #driver_service = Service('/Users/nicolasbenavides/Downloads/chromedriver')
-    driver = webdriver.Chrome(service=BraveService(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()), options=options)
+    driver = webdriver.Chrome(service=driver_service, options=options)
     print('Selenium driver instance created')
     return driver
-
 
 # Jumbo
 def jumbo_extract(driver):
@@ -95,7 +94,7 @@ def jumbo_extract(driver):
     
     df = pd.DataFrame(main_list)
     df = df.drop_duplicates()
-    df['Descuento'] = df['Descuento'].str.replace(' ', '', regex=True)
+    df['Descuento'] = df['Descuento'].apply(lambda x: x.replace(' ', '') if not pd.isna(x) else float('nan'))
     print(f'{len(df)} {df.Store[0]} items was extracted')
     driver.quit()
     return df
@@ -216,7 +215,7 @@ def carulla_extract(driver):
         nombre = detail.find_element(By.CLASS_NAME, "vtex-product-summary-2-x-productBrand").text 
       
         precio_element =  detail.find_elements(By.CLASS_NAME, "exito-vtex-components-4-x-currencyContainer")
-        if len(precio_element) == 3:
+        if len(precio_element) > 2 :
             precio = precio_element[2].text
         else:
             precio = precio_element[1].text
@@ -228,7 +227,7 @@ def carulla_extract(driver):
         main_list.append({'Nombre': nombre, 'Descuento': descuento, 'Precio':precio, 'Store': 'Carulla'})       
     
     df = pd.DataFrame(main_list)
-    df['Descuento'] = df['Descuento'].apply(lambda x: (x.str[0] + x.str[1:]) if not pd.isna(x) else float('nan'))
+    df['Descuento'] = df['Descuento'].apply(lambda x: x.replace(' ', '') if not pd.isna(x) else float('nan'))
     print(f'{len(df)} {df.Store[0]} items was extracted')
     driver.quit()
     return df
@@ -250,11 +249,11 @@ def exito_extract(driver):
     action.click(wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "css-yiuvdt")))[1]).perform()
     action.click( wait.until(EC.element_to_be_clickable((By.XPATH, '//div[contains(text(), "Colina")]')))).perform()
     action.click( wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Confirmar"]')))).perform()
-    sleep(random.uniform(2,3))
+    sleep(random.uniform(3.5,4))
     action.click( wait.until(EC.presence_of_element_located((By.XPATH, '//div[text()="Sub-categoría"]')))).perform()
-    sleep(random.uniform(2,3))
+    sleep(random.uniform(3.5,4))
     action.click( wait.until(EC.presence_of_element_located((By.ID, 'category-3-ginebra')))).perform()
-    sleep(random.uniform(2,3))
+    sleep(random.uniform(4, 5))
     for _ in range(4):
         driver.execute_script("window.scrollBy(0, 500);")
         sleep(random.uniform(2, 3))
@@ -288,7 +287,7 @@ def exito_extract(driver):
      
     df = pd.DataFrame(main_list)
     print(f'{len(df)} {df.Store[0]} items was extracted')
-    df['Descuento'] = df['Descuento'] = df['Descuento'].str.replace(' ', '', regex=True)
+    df['Descuento'] = df['Descuento'].apply(lambda x: x.replace(' ', '') if not pd.isna(x) else float('nan'))
     driver.quit() 
     return df
     
